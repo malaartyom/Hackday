aed7a5db53614cda92cb99ae21e415565bc74eb1
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2023-02-25 11:40:39 +0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,5 @@
 class Z7_num:
+
     def __init__(self, num: int):
         if num >= 0:
             self.value = num % 7
@@ -19,5 +20,29 @@ class Z7_num:
     
     def __neg__(self):
         return Z7_num(-self.value)
-    
 
+
+class Z7_Matrix:
+    
+    def __init__(self, num, *args):
+        self.size = num
+        self.value = [[Z7_num(0) for i in range(num)] for j in range(num)]
+
+        if args:
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.value[i][j] = args[i][j]
+    
+    def __str__(self) -> str:
+        return_string = ""
+        for i in range(self.size):
+            for j in range(self.size):
+                return_string += Z7_num.__str__(self.value[i][j])
+                return_string += " "
+            return_string += "\n"
+        return return_string[:-1]
+
+
+if __name__ == "__main__":
+    A = Z7_Matrix(3)
+    print(A)
