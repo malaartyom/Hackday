f009f0ccf2abc5cb1f7b0e265f23b4b714a7f390
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2023-02-25 10:59:48 +0700

    Make class of Z7 number

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,21 @@
-a = input()
+class Z7_num:
+    def __init__(self, num: int):
+        if num >= 0:
+            self.value = num % 7
+        else: 
+            self.value = 7 - ((-num) % 7)
+    
+    def __str__(self) -> str:
+        return str(self.value)
+    
+    def __add__(self, other):
+        return Z7_num(self.value + other.value)
+
+    def __mul__(self, other):
+        return Z7_num(self.value * other.value)
+    
+    def __sub__(self, other):
+        return Z7_num(self.value - other.value)
+    
+    def __neg__(self):
+        return Z7_num(-self.value)
